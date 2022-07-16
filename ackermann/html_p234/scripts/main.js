a3c53d95b76538034840bcd374e12aa020ba201a
mkrpooh <k.robold@hotmail.de>
2022-07-17 00:31:46 +0200

    "00:31"

---
+++ ackermann/html_p234/scripts/main.js
@@ -0,0 +1,11 @@
+function compareNumbers(numberAtI, numberAtJ) {
+    if(numberAtI === numberAtJ) {
+        console.log(`Doppelte Zahl gefunden: ${numberAtI}`);
+        document.write(`Doppelte Zahl gefunden: ${numberAtI}`);
+    }
+}
+function checkNumber(numberAtI, numbers, i) {
+    for(let j = j + 1; j < numbers.length; j++) {
+        const numberAtJ
+    }
+}
