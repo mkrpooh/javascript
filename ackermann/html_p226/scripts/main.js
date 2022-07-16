c41ae67c9c4b8f2bc9a2e82e11b9fe2d0c44867c
mkrpooh <k.robold@hotmail.de>
2022-07-16 02:29:28 +0200

    "02:29"

--- ackermann/html_p226/scripts/main.js
+++ ackermann/html_p226/scripts/main.js
@@ -2,12 +2,13 @@ function findDuplicates() {
     const numbers = [2, 4, 5, 2, 5, 8, 5, 4711];
     for(let i = 0; i < numbers.length; i++) {
         const numberAtI = numbers[ i ];
-        for(let J = 0; j < numbers.length; j++) {
+        for(let j = i + 1; j < numbers.length; j++) {
             const numberAtJ = numbers[ j ];
             if(numberAtI === numberAtJ) {
                 console.log(`Doppelte Zahl gefunden: ${numberAtI}`);
-                document.write(`Doppelte Zahl gefunden: ${numberAtI}`);
+                document.write(`Doppelte Zahl gefunden: ${numberAtI} <br />`);
             }
         }
     }
 }
+//document.addEventListener('DOMContentLoaded', findDuplicates);
